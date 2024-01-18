3579a21284047c869380f3ce6059397ee8ad31bb
eliocamacaro <149683434+eliocamacaro@users.noreply.github.com>
2024-01-18 15:28:03 +0000

    Done

--- src/app.py
+++ src/app.py
@@ -6,6 +6,12 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
+client_id = os.environ.get("473ab671b245469fa98250d34ce26723")
+client_secret = os.environ.get("7d35bd5f67714510ae40944dc2261104")
+
+import spotipy
+spotipy.Spotify()
+
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
